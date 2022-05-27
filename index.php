d787f453b4fa791ee117a7627881616a48a073e3
Pappy-Hub <boyshokan@gmail.com>
2022-05-27 03:01:52 +0100

    added lines of code

--- index.php
+++ index.php
@@ -1,49 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-        <title>Registration Form</title>
+    <title>Registration Form</title>
 </head>
+
 <body>
 
-<form method="POST" action="user_data.php"> 
+    <form method="POST" action="user_data.php">
         <div>
             <label for="name">
-                Name: <input type="text" name="name">
+                Name: <input type="text" name="fname" placeholder="Firstname Lastname" autocomplete="off" required>
             </label>
         </div>
 
         <div>
             <label for="email">
-                Email: <input type="email" name="email">
+                Email: <input type="email" name="email" autocomplete="off" required>
             </label>
         </div>
         <div>
             <label for="dob">
-                Date of Birth: <input type="dob" name="dob" placeholder=" Example 12/12/2022">
+                Date of Birth: <input type="date" name="dob" placeholder="12/12/2022" autocomplete="off" required>
             </label>
         </div>
         <div>
             <label for="gender">
-                Gender: <input type="text" name="gender">
+                Gender: <input type="text" name="gender" autocomplete="off" required>
             </label>
         </div>
         <div>
             <label for="country">
-                Country: <input type="text" name="country">
+                Country: <input type="text" name="country" autocomplete="off" required>
             </label>
         </div>
 
         <div>
-            <button type="submit" >Submit</button>
+            <button type="submit">Submit</button>
         </div>
     </form>
 
 
-    
+
 
 
 
 
 
 </body>
+
 </html>
--- user_data.php
+++ user_data.php
@@ -1,34 +1,19 @@
 <?php
 
-// print_r($_POST);
+extract($_REQUEST);
+$filename = fopen("userdata.csv", "a"); //open the filename in a read mode
 
-// $name = "name";
-// $email = "email";
-// $dob = "dob";
-// $gender = "gender";
-// $country = "country";
+fwrite($filename, "Name :");
+fwrite($filename, $fname . "\n");
+fwrite($filename, "Email :");
+fwrite($filename, $email . "\n");
+fwrite($filename, "Date of birth :");
+fwrite($filename, $dob . "\n");
+fwrite($filename, "Gender :");
+fwrite($filename, $gender . "\n");
+fwrite($filename, "Country :");
+fwrite($filename, $country . "\n");
 
-$name = $_POST['name']; 
-$email = $_POST['email'];
-$dob = $_POST['dob'];
-$gender = $_POST['gender'];
-$country = $_POST['country'];
-
-echo 'NAME: ' . $name . "<br>";
-echo 'EMAIL: ' . $email . "<br>";
-echo 'DOB: ' . $dob . "<br>";
-echo 'GENDER: ' . $gender . "<br>";
-echo 'COUNTRY: ' . $country . "<br>";
-
-
-$filename = "./userdata.csv"; //file path
-$handle = fopen($filename, "a"); //open the file in a read mode
-
-fwrite($handle, "Pappy is the man, shit pays \n"); //Write to file
-
-fclose($handle); //close file
-
-
-
+fclose($filename); //close filename
 
 ?>
